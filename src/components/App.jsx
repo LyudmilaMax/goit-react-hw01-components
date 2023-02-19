175f67f55a9b242751ef04289022f63da68a3f65
Lyudmila Maximenko <109598383+LyudmilaMax@users.noreply.github.com>
2023-02-19 13:19:51 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,12 @@
-import Profile from './Profile/Profile'
-import user from './Profile/user.json'
-import Statistics from './Statistics/Statistics' 
-import data from './Statistics/data.json'
-import FriendList from './Friends/FriendsList'
-import friends from './Friends/friends.json'
-import TransactionHistory from './Transactions/TransactionHistory'
-import transactions from './Transactions/transactions.json'
+import Profile from './Profile/Profile';
+import user from './Profile/user.json';
+import Statistics from './Statistics/Statistics';
+import data from './Statistics/data.json';
+import FriendList from './Friends/FriendsList';
+import friends from './Friends/friends.json';
+import TransactionHistory from './Transactions/TransactionHistory';
+import transactions from './Transactions/transactions.json';
 import { Container } from './App.styled';
-
 
 export const App = () => {
   return (
